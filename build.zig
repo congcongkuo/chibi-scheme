2014f657d18b77e87f9cbdb10513279f4f5808fc
Congcong Kuo <congcong.kuo@gmail.com>
2025-04-01 22:12:31 +0800

    5th

--- build.zig
+++ build.zig
@@ -18,6 +18,7 @@ pub fn build(b: *std.Build) void {
         .root_module = b.createModule(.{
             .target = target,
             .optimize = optimize,
+            .link_libc = true,
         }),
     });
 
@@ -85,6 +86,7 @@ pub fn build(b: *std.Build) void {
         .root_module = b.createModule(.{
             .target = target,
             .optimize = optimize,
+            .link_libc = true,
         }),
     });
 
